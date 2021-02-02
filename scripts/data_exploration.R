639ca49a0c988ec8dde1a3e74f428573dceca17e
keatonwilson <keatonwilson@me.com>
2021-02-02 07:27:39 -0700

    small add to data exploration script before new branch

--- scripts/data_exploration.R
+++ scripts/data_exploration.R
@@ -14,3 +14,4 @@ dim(test_df$lat)
 
 # Each file contains a matric of coords (lat or lon), that is 6964 x 1441
 # Each of the 20 sets of files is a simulation replication. 
+# Each Column is an hour (24*2*30 +1 )
