5ef438e6e0acbbdf8e61fcd148337c18105400f1
keatonwilson <keatonwilson@me.com>
2021-02-02 07:48:42 -0700

    initalizing UI

--- ui.R
+++ ui.R
@@ -1,33 +1,31 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# UI
+# Keaton Wilson & Noah Giebink
+# keatonwilson@me.com
+# 2021-02-02
+
+
+# packages ----------------------------------------------------------------
 
 library(shiny)
+library(bslib)
 
+
+# Shiny UI ---------------------------------------------------------------
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(
+    fluidPage(
+        theme = bs_theme(bootswatch = 'spacelab'),
+        navbarPage(title = "Ecostructure Larval Dispersal",
+                   id = "navbar", 
+                   
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# Front Mapping Panel -----------------------------------------------------
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+                   tabPanel("Mapping", 
+                            value = "tab1", 
+                            fluid = TRUE
+                       
+                   )
+                   )
     )
-))
+)
