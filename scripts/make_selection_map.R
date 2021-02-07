a122416deed34b2ec58a914070eba58f35cd0c72
keatonwilson <keatonwilson@me.com>
2021-02-07 08:21:30 -0700

    initialized basic UI

---
+++ scripts/make_selection_map.R
@@ -0,0 +1,19 @@
+# Selection Mapping Function
+# Keaton Wilson
+# keatonwilson@me.com
+# 2020-02-05
+
+# packages
+library(tidyverse)
+library(leaflet)
+
+make_selection_map = function(data = NULL) {
+  # leaflet function to generate map
+  leaflet() %>%
+    addTiles()
+    # addCircleMarkers(~lon, ~lat,
+    #                  stroke = FALSE,
+    #                  fillOpacity = 0.6, 
+    #                  opacity = 0.5
+    # )
+}
--- server.R
+++ server.R
@@ -5,8 +5,25 @@
 
 library(shiny)
 
+
+# Sourcing Scripts --------------------------------------------------------
+source("./scripts/make_selection_map.R")
+source("./scripts/make_simulation_map.R")
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
+
+# Map Panel to Make Location Selection ------------------------------------
+output$selection_map = renderLeaflet({
+  make_selection_map()
+})
+
+
+# Map Panel to View Simulation --------------------------------------------
+
+output$simulation_map = renderLeaflet({
+  make_simulation_map()
+})
 
 })
